cb0144057b5cbf43e5ed152be5ff23089cc4d966
jayak <kumarijaya127@gmail.com>
2024-05-01 21:51:25 +0530

    add post model

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const userModel = require("./model/user");
+const postModel = require("./model/posts");
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
@@ -22,18 +23,41 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
-app.post("/create", (req, res) => {
-  let { username, email, password } = req.body;
+// app.post("/create", (req, res) => {
+//   let { username, email, password } = req.body;
+//   bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, async (err, hash) => {
+//       let createdUser = await userModel.create({
+//         username,
+//         email,
+//         password: hash,
+//       });
+//       let token = jwt.sign({ email }, "secret");
+//       res.cookie("accessToken", token);
+//       res.send(createdUser);
+//     });
+//   });
+// });
+
+app.post("/register", async (req, res) => {
+  let { username, name, age, email, password } = req.body;
+
+  let user = await userModel.findOne({ email });
+  if (user) return res.status(500).send("User already registered");
+
   bcrypt.genSalt(10, (err, salt) => {
     bcrypt.hash(password, salt, async (err, hash) => {
-      let createdUser = await userModel.create({
+      let user = await userModel.create({
         username,
+        name,
+        age,
         email,
         password: hash,
       });
-      let token = jwt.sign({ email }, "secret");
+
+      let token = jwt.sign({ email, userId: user._id }, "secret");
       res.cookie("accessToken", token);
-      res.send(createdUser);
+      res.send("registered");
     });
   });
 });
